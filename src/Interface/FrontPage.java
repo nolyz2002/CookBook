a4130651d3a59ae705693e3eaa6dd0423f56e794
howen <howenolan30@gmail.com>
2023-02-02 23:54:03 -0600

    Moved Jframe out of run method

--- src/Interface/FrontPage.java
+++ src/Interface/FrontPage.java
@@ -1,16 +1,19 @@
 package Interface;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.io.IOException;
 import javax.swing.*;
 import java.awt.*;
 
-public class FrontPage {
+public class FrontPage  {
     private JPanel panel1;
     private JScrollPane scrollPane;
     private JScrollPane otherUserRecipes;
     private JButton button1;
 
 
+    JFrame f = new JFrame("Test HTML");
     JEditorPane jep = new JEditorPane();
 
 
@@ -28,17 +31,27 @@ public class FrontPage {
         scrollPane.setPreferredSize(new Dimension(250,900));
         otherUserRecipes.setPreferredSize(new Dimension(250,250));
 
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                UserRecipes ur = new UserRecipes();
+                f.dispose();
+
+            }
+        });
+
         run();
+
     }
+
 
     public void run(){
 
 
-        JFrame f = new JFrame("Test HTML");
+
 
 
         f.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        //f.getContentPane().add(scrollPane).setSize(100,100);
         f.add(panel1);
         f.setSize(new Dimension(1920,1080));
         f.setVisible(true);
