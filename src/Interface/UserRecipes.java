360cf545dd8fe23db37eb43e5a1411ec493a411b
howen <howenolan30@gmail.com>
2023-02-04 22:55:31 -0600

    Added a back button which takes user back to the front page

--- src/Interface/UserRecipes.java
+++ src/Interface/UserRecipes.java
@@ -2,15 +2,31 @@ package Interface;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
 
 public class UserRecipes {
     private JPanel panel1;
     private JTextArea labelTextArea;
     private JTextArea labelTextArea1;
     private JButton addButton;
+    private JButton backButton;
     public JFrame frame = new JFrame();
     public UserRecipes(){
         run();
+        backButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    FrontPage frame1 = new FrontPage();
+                    frame.dispose();
+                } catch (IOException ex) {
+                    throw new RuntimeException(ex);
+                }
+
+            }
+        });
     }
 
     public void run(){
